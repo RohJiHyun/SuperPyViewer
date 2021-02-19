8b492288015640dcc9b6661005f04968ee0c268c
ROH <rohjihyun95@gmail.com>
2021-02-19 17:20:25 +0900

    add viewer

--- pyviewer/viewer.py
+++ pyviewer/viewer.py
@@ -24,7 +24,7 @@ from pyviewer import datacontainer as dc
 
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QTextEdit, QDockWidget, QListWidget)
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from pyviewer import ui
 from pyviewer import viewcontrolobj as vco 
@@ -35,20 +35,40 @@ class CustomViewer(QMainWindow):
     def __init__(self, title, width, height):
         super().__init__()
         self.resize(width, height)
+        # self.layout = BorderLayout()
         self.windows = []
         self.world = dc.WorldContainer()
-        self.initUI()
+        # self.initUI()
 
-    def add_window(self, window, isdock=True, name=""):
+    def add_window(self, window, isdock=True, name="", allowed_area=Qt.AllDockWidgetAreas, central=False):
         """
             window is QT Window Object. and window_object has lisener
         """
-        if isdock : 
+        print("centerla" , central)
+        if isdock and not central : 
             dock = QDockWidget(name, self)
             dock.setFloating(False)
             self.windows.append(dock)
-            self.addDockWidget(Qt.RightDockWidgetArea, dock)
+            dock.setAllowedAreas(allowed_area)
             dock.setWidget(window)
+            dock.widget().setMaximumSize(dock.widget().minimumSize())
+
+            # self.addDockWidget(Qt.RightDockWidgetArea, dock)
+            self.addDockWidget(allowed_area, dock)
+            
+        elif  central:
+            # dock = QDockWidget(name, self)
+            # dock.setFloating(False)
+            # self.windows.append(dock)
+            # dock.setAllowedAreas(allowed_area)
+            # dock.setWidget(window)
+            print("chels")
+
+            # self.addDockWidget(Qt.RightDockWidgetArea, dock)
+            # self.addDockWidget(allowed_area, dock)
+            self.setCentralWidget(window)
+            
+            
 
 
     def initUI(self):
@@ -56,6 +76,7 @@ class CustomViewer(QMainWindow):
         self.menu.initUI()
         self.Inspector = ui.InspectorUI(self)    
         self.Inspector.initUI()
+        
 
 
     def set_data(self, V, F):
@@ -63,18 +84,17 @@ class CustomViewer(QMainWindow):
         self.world.add_data(dc.DataContainer(V, F))
         
     def compile(self):
-        self._initialize()
+        self._initialize_mainview()
+
+        self.initUI()
 
 
-    def _initialize(self):
+    def _initialize_mainview(self):
         win = vco.Window("tit")
         win.set_world(self.world)
-        self.add_window(win, isdock=True, name=win.name)
+        self.add_window(win, isdock=True, name=win.name, central=True)
+        
     
-
-
-    def add_menubar(self):
-        pass
 
     def run(self):
         self.show()
@@ -90,6 +110,7 @@ class CustomViewer(QMainWindow):
 
 
 if __name__ == "__main__":
+    # V, F = igl.read_triangle_mesh("pyviewer/cube.obj")
     V, F = igl.read_triangle_mesh("pyviewer/cube.obj")
     # a = Viewer("title", 800, 900)
     # a.set_data(V,F)
